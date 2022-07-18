0fad36db55c8b381aabc41f9632dae2e7b4d062e
Shehroz <raxmatovshehroz8@gmail.com>
2022-07-18 20:11:12 +0500

    spa keyingi bosqich

---
+++ src/components/CategoryList.js
@@ -0,0 +1,8 @@
+// export default function CategoryList({catalog = []}) {
+    
+//     return(
+//         <div className="list">
+//             {catalog.map(el => ())}
+//         </div>
+//     )
+// }
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,19 @@
-
+import { useState, useEffect } from "react"
+import { getAllCategories } from "../api"
+import CategoryList from "../components/CategoryList"
+import {Loader} from "../components/Loader"
 export default function Home() {
+
+  const [catalog, setCatalog] = useState([])
+  useEffect(()=>{
+    getAllCategories().then(data =>{
+      setCatalog(data.categories)
+    })
+  }, [])
     return (
-      <h1>Hoem</h1>
+      <>
+        {!catalog.length ? (<Loader />) : (<CategoryList catalog={catalog}/>)}
+      </>
     )
   }
   
